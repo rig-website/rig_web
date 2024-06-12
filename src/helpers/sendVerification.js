2d08613378357be644efd7ef8eda8bb2fd79cf28
X99ror <xgeror0062@gmail.com>
2024-06-12 16:23:39 +0530

    second commit

--- src/helpers/sendVerification.js
+++ src/helpers/sendVerification.js
@@ -3,13 +3,13 @@ import { resend } from "@/lib/resend";
 
 import VerificationEmail from "../../emails/verificationmail";
 
-export async function sendVerificationEmail(email,username,verifyCode){
+export async function sendVerificationEmail(email,rigid){
     try {
         await resend.emails.send({
             from: 'onboarding@resend.dev',
             to: email,
             subject: 'verification code',
-            react: VerificationEmail(username, verifyCode),
+            react: VerificationEmail(rigid),
           });
         
     } catch (emailError) {
