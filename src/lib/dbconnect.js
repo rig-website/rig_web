980414e6cbe2cb8294808011f328c05db2d110a7
nirmal <nirmalfageria15@gmail.com>
2024-09-08 19:57:15 +0530

    fixing

--- src/lib/dbconnect.js
+++ src/lib/dbconnect.js
@@ -1,9 +1,3 @@
-// Load environment variables and encode them
-const username = encodeURIComponent(process.env.username);
-const password = encodeURIComponent(process.env.password);
-
-// Check that the environment variables are correctly loaded
-console.log('Username:', process.env.username, 'Password:', process.env.password);
-
+const {username , password} = process.env;
 // MongoDB connection string
-export const db = `mongodb+srv://${username}:${password}@cluster0.6obwzbq.mongodb.net/rig?retryWrites=true&w=majority&appName=Cluster0`;
+export const db =`mongodb+srv://${username}:${password}@cluster0.6obwzbq.mongodb.net/rig?retryWrites=true&w=majority&appName=Cluster0`; 
