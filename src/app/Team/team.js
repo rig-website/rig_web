4a8f09b60e45cbf7680584d469b57c96699401ca
Nirmalfageria <nirmal_b221082ee@nitc.ac.in>
2024-12-05 21:50:25 +0530

    team page updated

---
+++ src/app/Team/team.js
@@ -0,0 +1,205 @@
+
+    const teamMembers = [
+     
+      {
+        "name": "Shibin Fazil",
+        "linkedin": "https://www.linkedin.com/in/shibin-fazil/",
+        "presentation": "https://drive.google.com/uc?export=view&id=11WCYgS-fWlJtluoJtZz6AwTl0CZZ7Rhx",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Karthik Jayaraj",
+        "linkedin": "https://www.linkedin.com/in/karthik-jayaraj-93a903222/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1dr4W01gyS4llPOgJjkRI2l647jUN1IEP",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Nikita S",
+        "linkedin": "https://www.linkedin.com/in/nikitha-saji/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1qjwhbprZWhIr40Cj7qCc2fu5wlUTyE-W",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Karthik Manoranjan",
+        "linkedin": "https://www.linkedin.com/in/karthik-manoranjan",
+        "presentation": "https://drive.google.com/uc?export=view&id=1DDWoUqwW0aBAe7IazhGnF_hV9WJ_ZFnq",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Sneha Joe M",
+        "linkedin": "https://www.linkedin.com/in/sneha-joe-m-electronics-engineer",
+        "presentation": "https://drive.google.com/uc?export=view&id=1UiciK7sOUlBJRXNgLjR3waMbOWzvCDot",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Madhav Menon",
+        "linkedin": "https://www.linkedin.com/in/madhav-menon-",
+        "presentation": "https://drive.google.com/uc?export=view&id=1QHIXhIFFu5MLFSwJqI8Sde_nnuTN1f4Y",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Vishakha",
+        "linkedin": "https://www.linkedin.com/in/vishakha--sharma",
+        "presentation": "https://drive.google.com/uc?export=view&id=1dA2g3o2zNlgzWnLPRjOiwUd0iRUgk7OG",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+       
+      {
+        "name": "Adhyuth Narayan",
+        "linkedin": "https://www.linkedin.com/in/adhyuth-narayan",
+        "presentation": "https://drive.google.com/uc?export=view&id=1JbAkmFDPiO79KE_hevZRw_9VlaEZittM",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Aadhil Noushad",
+        "linkedin": "https://www.linkedin.com/in/aadhil-noushad-179849316/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1s_MZj-vYIhYX-89qrz16GkF5QSrvZX0q",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Pranav Ramesh",
+        "linkedin": "https://www.linkedin.com/in/pranav-ramesh-36b24b244/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1dsmuehQ25ktgnI3dNC84ckXA9c8UijUz",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Krishnaprasad",
+        "linkedin": "/",
+        "presentation": "",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Prudhvi",
+        "linkedin": "https://www.linkedin.com/in/prudhvi-mallavarapu78/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1m7BbmAvxUuC0pDvgNs8As2iaesoNKL0I",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Kathaan Vora",
+        "linkedin": "https://www.linkedin.com/in/kathaan-vora-845b6b219/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1QCkm0SS6we6uu2t3QP46rhyHBfoRqIWh",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Aftaab Ahamed",
+        "linkedin": "https://www.linkedin.com/in/aftaab-ahamed-058b9b221/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1lR0sqQr3f1fQthO9xpZI1vWfFu-REfCp",
+        "year": "2021-2025",
+        "position": "Team Member"
+      },
+      {
+        "name": "Nirmal Fageria",
+        "linkedin": "https://www.linkedin.com/in/nirmal-fageria-11bab125a",
+        "presentation": "https://drive.google.com/uc?export=view&id=1WU-eJz315sUZFdje91Kfqf-HnxqRoOrR", 
+        "year": "Developer",
+        // "position": "Developer"
+      },
+      {
+        "name": "Chandrabhusan Kumar",
+        "linkedin": "/",
+        "presentation": "/CB.png",
+        "year": "Developer",
+        // "position": "Developer and Designer"
+      },
+      {
+        "name": "Himanshu",
+        "linkedin": "/",
+        "presentation": "",
+        "year": "Developer",
+        // "position": "Developer"
+      },
+      {
+        "name": "Indulekha S Kumar",
+        "linkedin": "https://www.linkedin.com/in/indulekha-s-kumar-3a5b3a190/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1nSR7AAgEjsPPuDMCCGkNb5qOesrcmGfw",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Pippala Kamala Krishna",
+        "linkedin": "https://www.linkedin.com/in/kamala-krishna/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1vsHNp2ocJU3lDIwig7yn6SuaWUiWAyEK",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Erin Jossy",
+        "linkedin": "https://www.linkedin.com/in/erin-jossy-16105518b/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1RqNgIPmcqlkfc3xxQFFdZ6vrUNvMnIpO",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Bandaru Jashwanth",
+        "linkedin": "https://www.linkedin.com/in/jashwanth-bandaru-b6305018b/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1pg-inmeBXd1boCcf-bwKxYBnt8IDRm0g",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Amirul Haqe",
+        "linkedin": "https://www.linkedin.com/in/amirul-haqe/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1QeCb7VHRphJUa34HO_mr-nnKWytCJDRT",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Ajin J",
+        "linkedin": "https://www.linkedin.com/in/ajin-j/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1odMeu8sdPPDxPKFrGKEuNQI7ibUKkGPf",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Jahnavi Dadireddy",
+        "linkedin": "https://www.linkedin.com/in/jahnavi-dadireddy-938654190/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1ucWt1_kD_eJFjWVmdUMYI4jZdeCXlWqI",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Abel C Dixon",
+        "linkedin": "https://www.linkedin.com/in/abelcdixon/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1pjJKOb0N5QfonB1c_O1haGms73hIEkcQ",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Somu Bala Upendra Reddy",
+        "linkedin": "https://www.linkedin.com/in/somu-upendra-reddy-795527190/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1wPGC46Ytk5JcTu5AH3juHM-0cHs5ScwZ",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Muhammad Basil V",
+        "linkedin": "https://www.linkedin.com/in/muhammad-basil-v/",
+        "presentation": "https://drive.google.com/uc?export=view&id=18SKAUDtqaJy5peyhkdEbmYXUp9V4O3cL",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      {
+        "name": "Sompalli Praneetha",
+        "linkedin": "https://www.linkedin.com/in/praneetha-sompalli-688690212/",
+        "presentation": "https://drive.google.com/uc?export=view&id=1ZYg5hhZ4cHM2qNNjfpr0kB8GTOw6s8tc",
+        "year": "2019-2023",
+        "position": "Team Member"
+      },
+      
+    
+    ];
+  
+  export default teamMembers;
