22c541423e900456c7d4b7f4f537ac31b037edff
CB <chandrabhushankumar138@gmail.com>
2025-01-24 21:42:34 +0530

    footer fixed

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -14,6 +14,7 @@ import axios from 'axios';
 const Navbar = () => {
   const [isSidebarVisible, setSidebarVisible] = useState(false);
   const pathname = usePathname();
+  const isActive = (path) => pathname === path;
   const router = useRouter();
   const dispatch = useDispatch();
   const isLogin = useSelector((state) => state.auth.isLogin);
